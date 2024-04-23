ee4d8a0048b8ce33c15e88742f0208bbfc494bbe
vipula-anil <vipula00@gmail.com>
2024-04-23 17:35:18 -0500

    Days calculated

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,23 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+let determination;
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+let milesPerKilometer = 0.621
+
+console.log(typeof shuttleSpeed);
+console.log(typeof distanceToMars);
+console.log(typeof distanceToMoon);
+console.log(typeof milesPerKilometer);
+
+let milesToMars = distanceToMars * milesPerKilometer;
+let hoursToMars = milesToMars/shuttleSpeed;
+let daysToMars = hoursToMars/24;
+console.log("Determination will take " + daysToMars + "  days to reach Mars!")
+
+let milesToMoon = distanceToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon/shuttleSpeed;
+let daysToMoon = hoursToMoon/24;
+console.log("Determination will take "+ daysToMoon + " days to reach Moon");
