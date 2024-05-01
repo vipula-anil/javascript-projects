a2bca26403b5f0603874e02755625026aea8eafe
vipula-anil <vipula00@gmail.com>
2024-05-01 16:18:48 -0500

    Errors and Debugging

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -3,9 +3,11 @@
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
 let launchReady = false;
-let fuelLevel = 17000;
+let fuelLevel = 27000;
 let crewStatus = true;
 let computerStatus = 'green';
+let crewReady = false;
+//let fuelCheck = false;
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
@@ -14,15 +16,41 @@ if (fuelLevel >= 20000) {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
 }
-
 console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+console.log("crewReady = ", crewReady);
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
+
+
+/*if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   fuelCheck = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   fuelCheck = false;
+}
+
+//console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green' && fuelCheck){
+   console.log('Fuel,Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Fuel, Crew or computer not ready!');
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("launchReady = ", launchReady);*/
