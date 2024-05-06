c10147dd6a92fd0d527a57b073b8bcc9a8ac8f02
vipula-anil <vipula00@gmail.com>
2024-05-06 17:04:34 -0500

    method chaining

--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -6,6 +6,12 @@ console.log(word.toUpperCase());
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
-
+console.log(word.slice(4).toUpperCase());
 
 //Experiment with other combinations (chains) of string methods.
+let language = " JavaScript world ";
+console.log(language.replace('J', 'Q'));
+console.log(language.slice(0,5));
+console.log(language.trim());
+console.log(language.indexOf('world'));
+console.log(language);
