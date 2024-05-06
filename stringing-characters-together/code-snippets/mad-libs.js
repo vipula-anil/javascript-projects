e6bbdd872fecc39f0abcde812bebf10a274de239
vipula-anil <vipula00@gmail.com>
2024-05-06 16:32:39 -0500

    bracket notation

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "boats";
+let name = "vipula";
+let verb = "running";
+let adjective = "fast";
+let color = "red";
 
-console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`);
