d7ea3a2ed64d63a7b2ac53b3d6e4a3c6d7f302c3
vipula-anil <vipula00@gmail.com>
2024-05-08 18:53:19 -0500

    part six arrays completed

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,41 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
 
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+let table =[];
+table.push(element1,element2,element26);
+console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+ console.log(table[1], table[1][1]);
 
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[0][2],table[1][0],table[2][1]);
 
-//5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+//5) 'table' is an example of a 2-dimensional array. 
+//The first “level” contains the element arrays, and the second level holds the name/symbol/mass values.
+// Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+let table1 = [];
+let element11 = [];
+let element21 = [];
+
+table1.push(element11,element21);
+
+let ele1 = [5,7,8];
+let ele2 =[8,3,9];
+let ele3 =[9,3,1];
+
+element11.push(ele1,ele2,ele3);
+
+let ele11 = [6,4,6];
+let ele21 =[0,8,3];
+let ele31 =[9,3,5];
+
+element21.push(ele11,ele21,ele31);
+
+console.log(table1[0],table1[0][0],table1[0][0][0]);
--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,7 +9,7 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
-let determination;
+/*let determination;
 let shuttleSpeed = 17500;
 let distanceToMars = 225000000;
 let distanceToMoon = 384400;
@@ -28,4 +28,127 @@ console.log("Determination will take " + daysToMars + "  days to reach Mars!")
 let milesToMoon = distanceToMoon * milesPerKilometer;
 let hoursToMoon = milesToMoon/shuttleSpeed;
 let daysToMoon = hoursToMoon/24;
-console.log("Determination will take "+ daysToMoon + " days to reach Moon");
+console.log("Determination will take "+ daysToMoon + " days to reach Moon");*/
+
+console.log(Boolean ("true"));
+console.log(Boolean("TRUE"));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(''));
+
+console.log(0 == true);
+
+let billHasBeenPaid = false;
+
+if(!billHasBeenPaid){
+    console.log("Ypur bill is due soon");
+}
+
+let a = 7;
+
+if(a % 2 === 1) {
+    console.log("Launch");
+} else if(a > 5) {
+    console.log("Code");
+} else {
+    console.log("LaunchCode");
+} 
+
+let num = 7;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+    console.log("ODD");
+    }
+
+   // if (num > 0) {
+        //console.log("POSITIVE");
+    //}
+}
+
+
+
+let engineIndicatorLight = "redblinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCdode = 200;
+let shuttleSpeed = 15000;
+
+/*if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking"){
+    console.log("engines are prepairing to start");
+} else {
+    console.log("engines are off");
+}*/
+
+/*if (crewStatus) {
+    console.log("Crew Ready"); 
+} else ("Crew Not Ready");
+
+if (computerStatusCdode === 200) {
+    console.log("Please Stand By. Computer Is Rebooting");
+} else if (computerStatusCdode === 400){
+    console.log("Success! Computer Online.");
+} else {
+    console.log("Alert: Computer Offline.");
+}
+
+if (shuttleSpeed > 17500) {
+    console.log("ALERT: Escape velocity reached!");
+} else if (shuttleSpeed < 8000) {
+    console.log("ALERT: cannot maintain orbit! ");
+} else {
+    console.log("Stable speed");
+}*/
+
+
+
+if (crewStatus && computerStatusCdode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+} else {
+    console.log("WARNING: Not ready.");
+}
+
+if (!crewStatus || computerStatusCdode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING: Not ready.");
+} else {
+    console.log("All systems go.");
+}
+/*
+
+if (crewStatus = true) {
+    console.log("Crew Ready.");
+} else {
+    console.log("Crew Not Ready.");
+}
+
+if (computerStatusCdode === 200) {
+    console.log("Please stand by. Computer is rebooting");
+} else if (computerStatusCdode === 400) {
+    console.log("Success! Computer online.");
+} else {
+    console.log("ALERT: Computer offline.");
+}
+
+
+//let day = Wednesday;
+
+//console.log(day);
+
+//let name = "Julie";
+
+//console.log("Hello", name);
+
+
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+    console.log('Fuel level cleared.');
+    launchReady = true;
+} else {
+    console.log('WARNING:Insufficient fuel!');
+    launchReady = false;
+}*/
