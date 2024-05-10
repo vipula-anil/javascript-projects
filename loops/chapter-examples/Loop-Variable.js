1c70a458aade024a23a825ef659b4a50b421aa15
vipula-anil <vipula00@gmail.com>
2024-05-10 14:37:12 -0500

    loop variables completed

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,5 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
+for (let i = 3; i < 51; i=i+5) {
    console.log(i);
  }
