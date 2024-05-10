3604ea0442783fc5f635bd897a3147970d81ecaa
vipula-anil <vipula00@gmail.com>
2024-05-10 13:37:03 -0500

    for loop with array completed

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let nameArray = ["Anil", "Vipula", "Anvin", "Books"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < nameArray.length; i++){
+    console.log(nameArray[i]);
+}
