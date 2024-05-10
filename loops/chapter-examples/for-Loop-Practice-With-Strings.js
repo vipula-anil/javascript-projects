76da94ad5550927b07e9919c2acef96875ce5784
vipula-anil <vipula00@gmail.com>
2024-05-10 13:29:17 -0500

    for loop practice with strings completed

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,7 @@
 // Create a string variable containing your name.
-
+let name = "vipulaanil";
 
 // Write a for loop that prints each character in your name on a different line.
+for(let i = 0; i < name.length; i++ ){
+    console.log(name[i]);
+}
