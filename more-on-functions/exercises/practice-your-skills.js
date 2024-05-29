c213f07384d0977f793438520e0fa22d7c97fe81
vipula-anil <vipula00@gmail.com>
2024-05-29 18:12:15 -0500

    function exercises

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,6 +6,17 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+let practice = function(myArg) {
+    if (typeof myArg === "number") {
+       return myArg * 3;
+    }else if (typeof myArg === "string") {
+        return "ARRR!";
+     } else {
+        return myArg;
+     }
+ };
+ console.log(practice('hhh'));
+
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +24,19 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+ practice = function(myArg){
+    if (typeof myArg === "number") {
+        return myArg * 3;
+     }else if (typeof myArg === "string") {
+         return "ARRR!";
+      } else {
+         return myArg;
+      }
+    };
+    
+    let newArray = arr.map(practice);
+    
+    console.log(arr);
+    console.log(newArray);
+
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -24,12 +24,30 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
+// Steal some fuel from the shuttle:
  
-//a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+let nonSuspiciousFunction = function(a) {
+  if (checkFuel(a) === 'green') {
+    return a - 100001;
+ }
+ else if (checkFuel(a) === 'yellow') {
+    return a - 50001;
+ }
+ else {
+    return a;
+ }
+};
 
-//b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
+let irs = function(levelOfFuel, itemsInCargo) {
+   arr = deckMops(itemsInCargo);
+  return `Raided ${nonSuspiciousFunction(fuelLevel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
+}
+/* 
+//a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. 
+The function takes one parameter. This will be the fuel level on the shuttle.
+
+//b). You must siphon off fuel without alerting the TAs. Inside your function, 
+you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
 //c). Once you figure out how much fuel to pump out, return that value.
 
@@ -40,13 +58,17 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
-//b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
+//b). You need to swipe two items from the cargo hold. Choose well. 
+Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
 
-//c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. You need to replace what you steal with something worthless. The count MUST stay the same, or you’ll get caught and thrown into the LaunchCode brig.
+//c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. 
+You need to replace what you steal with something worthless. The count MUST stay the same, 
+or you’ll get caught and thrown into the LaunchCode brig.
 
 //d). Don’t get hasty, matey! Remember to test your function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
+/* Finally, you need to print a receipt for the accountant. 
+Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
  * /
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
@@ -55,3 +77,4 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
 
+*/
