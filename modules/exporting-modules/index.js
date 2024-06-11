d546599f8c2aa6deef20dd280b7a142a0fcbf610
vipula-anil <vipula00@gmail.com>
2024-06-11 09:48:41 -0500

    exporting modules

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,17 @@
 // Import the modules exported from practiceExports.js below:
+// const palindromeCheck = require("./practiceExports.js");
+
+// console.log(typeof palindromeCheck);
+// console.log(palindromeCheck("that"));
+// console.log(palindromeCheck("radar"));
+
+const practice = require("./practiceExports.js");
+let arr = ["Hello", "World", 123, 987, "LC101"];
+// console.log(typeof practice);
+// console.log(practice);
+console.log(practice.isPalindrome("mom"));
+console.log(practice.evenOrOdd(9));
+
+for (let i = 0; i < 3; i++) {
+  console.log(practice.randomArrayElement(arr));
+}
--- modules/exporting-modules/practiceExports.js
+++ modules/exporting-modules/practiceExports.js
@@ -1,16 +1,22 @@
-function isPalindrome(str){
-    return str === str.split('').reverse().join('');
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
 
-function evenOrOdd(num){
-    if (num%2===0){
-        return "Even";
-    } else {
-        return "Odd";
-    }
+function evenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
 }
 
-function randomArrayElement(arr){
-    let index = Math.floor(Math.random()*arr.length);
-    return arr[index];
+function randomArrayElement(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
+
+module.exports = {
+  isPalindrome: isPalindrome,
+  evenOrOdd: evenOrOdd,
+  randomArrayElement: randomArrayElement,
+};
