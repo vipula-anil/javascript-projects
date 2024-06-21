69f6ae0a90ada3b630fc8cd63d56e912c87686ff
vipula-anil <vipula00@gmail.com>
2024-06-20 20:27:32 -0500

    Unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,17 @@
-
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+    if (num % 2 === 0) {
+      return "Launch!";
+    } else if (num % 3 === 0) {
+      return "Code!";
+    } else if (num % 5 === 0) {
+      return "Rocks!";
+    }
+  },
+};
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,36 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const launchcode = require("../index.js");
 
-describe("Testing launchcode", function(){
-
+describe("Testing launchcode", function () {
   // Write your unit tests here!
-  
+  test("Has a organization", () => {
+    expect(launchcode.organization).toContain("nonprofit");
+  });
+
+  test("Has a excecutive director", () => {
+    expect(launchcode.executiveDirector).toContain("Jeff");
+  });
+
+  test("Has a percentageCoolEmployees", () => {
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("programs offered has length of 3", () => {
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test("programs offered has three strings", () => {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+  });
+  test("when passed a number that is ONLY divisible by 2, return 'Launch!'", () => {
+    expect(launchcode.launchOutput(4)).toBe("Launch!");
+  });
+  test("when passed a number that is ONLY divisible by 3, return 'Code!'", () => {
+    expect(launchcode.launchOutput(6)).toBe("Code!");
+  });
+
+  test("When passed a number that is ONLY divisible by 5, return 'Rocks!'", () => {
+    expect(launchcode.launchOutput(25)).toBe("Rocks!");
+  });
 });
