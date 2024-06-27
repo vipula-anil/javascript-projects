b2ef75a2f7589c5e4f0a5db7031a8dd8d56678d0
vipula-anil <vipula00@gmail.com>
2024-06-27 15:50:57 -0500

    exception exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,52 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try {
+      result = lab.runLab(3);
+    } catch (err) {
+      result = "Error thrown";
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
 
 let studentLabs = [
   {
-    student: 'Carly',
+    student: "Carly",
     runLab: function (num) {
-        return Math.pow(num, num);
-    }
+      return Math.pow(num, num);
+    },
   },
   {
-    student: 'Erica',
+    student: "Erica",
     runLab: function (num) {
-        return num * num;
-    }
-  }
+      return num * num;
+    },
+  },
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+    student: "Blake",
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Jessica",
+    runLab: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Mya",
+    runLab: function (num) {
+      return num * num;
+    },
+  },
+];
+
+gradeLabs(studentLabs2);
